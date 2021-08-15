fc16d6cf3173a3b18c852dcff945b8bd1cbf2189
Nayanda Haberty <nayanda1@outlook.com>
2021-08-16 00:31:00 +0700

    Refactor

---
+++ Builder/Classes/Buildable.swift
@@ -0,0 +1,14 @@
+//
+//  GlobalFunction.swift
+//  Builder
+//
+//  Created by Nayanda Haberty on 16/08/21.
+//
+
+import Foundation
+
+public protocol Buildable {
+    init()
+}
+
+extension NSObject: Buildable { }
